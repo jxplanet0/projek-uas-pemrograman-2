ae069ce84a014f3f6d414f6d419a56861a3bd25d
jxplanet0 <fmanazili@gmail.com>
2023-05-30 19:32:28 +0700

    Update akhir.py
    mungkin perubahan final  pada code ini

--- program/selected/akhir.py
+++ program/selected/akhir.py
@@ -2,22 +2,21 @@ import random
 import string
 import exrex
 import re
+from string import ascii_lowercase
 
-list_forbidden = ['^','$','.','*','+','?','|']
+list_forbidden = ['^', '$', '.', '*', '+', '?', '|']
+
+print('\n')
 
 class PasswordGenerator:
     def __init__(self, length):
         self.__length = length
-  
-    def generate(self,limit):
+
+    def generate(self, limit):
         passwords = []
         while True:
-            password = ''.join(random.choice(string.punctuation + string.ascii_letters + string.digits) for _ in range(self.__length)) 
-            for i in password:
-                if i in  list_forbidden:
-                    i = "\\" + i
-                elif i == '\\':
-                    i = "\\\\" + i
+            password = ''.join(random.choice(string.punctuation + string.ascii_letters + string.digits) for _ in range(self.__length))
+            password = password.translate(str.maketrans('', '', ''.join(list_forbidden)))
             passwords.append(password)
             if len(passwords) == limit:
                 break
@@ -37,32 +36,53 @@ class PatternBasedPasswordGenerator(PasswordGenerator):
                 break
         return passwords
 
+lower_case = set(ascii_lowercase)  # set for faster lookup
+
+def find_regex(p):
+    cum = []
+    consecutive_count = 1
+    p = p.lower()
+
+    for i in range(len(p)):
+        if i > 0 and p[i] == p[i-1]:
+            consecutive_count += 1
+        else:
+            if p[i].isdigit():
+                cum.append(f"\d{{{consecutive_count}}}")
+            elif p[i] in lower_case:
+                cum.append(f"[a-z]{{{consecutive_count}}}")
+            else:
+                cum.append(re.escape(p[i]))
+            consecutive_count = 1
+
+    return ''.join(cum)
+
 def regex_pattern():
     options = ["Whitespace", "Alphabet", "Digit", "Others(symbols)"]
     regex_portion = []
-    regex_formula = [" ","[a-zA-Z]","\d"]
+    regex_formula = [" ", "[a-zA-Z]", "\\d"]
     print('\n')
-    
+
     for i, specification in enumerate(options, 1):
         print(i, specification)
         awalan = int(input(f"Masukkan jumlah {specification} yang anda inginkan: "))
-        
-        if i == 4:
-            if awalan > 0:
-                symbols = input("Masukkan 1 simbol apa saja yang ingin dimasukkan pada regex: ")
-                if symbols in list_forbidden:
-                    symbols = "\\" + symbols
-                elif symbols == '\\':
-                    symbols = '\\' + symbols
-                regex_formula.append(symbols)
-            
+
+        if i == 4 and awalan > 0:
+            symbols = input("Masukkan 1 simbol apa saja yang ingin dimasukkan pada regex: ")
+            if symbols in list_forbidden:
+                symbols = "\\" + symbols
+            elif symbols == '\\':
+                symbols = '\\' + symbols
+            regex_formula.append(symbols)
+
         regex_portion.append(awalan)
     print("\n")
     return regex_portion, regex_formula
 
+
 def generate_password(length):
     regex_portion, regex_formula = regex_pattern()
-    
+
     while length < sum(regex_portion):
         print("Total panjang regex melebihi panjang password yang diinginkan. Silakan coba lagi.")
         regex_portion, regex_formula = regex_pattern()
@@ -88,22 +108,28 @@ def generate_password(length):
 
         if len(regex) == length:
             break
-        
+
     print(regex_formula)
     print(regex)
-    modified_regex = [regex_formula[index-1] for index in regex]
+    modified_regex = [regex_formula[index - 1] for index in regex]
     pattern = ''.join(modified_regex)
     pattern = '^' + f'{pattern}' + '$'
-    print("\n", pattern)
+    print("\nGenerated passwords with pattern and regex modification", pattern)
 
-    limit_output = int(input("Masukkan berapa banyak output yang anda inginkan untuk ditampilkan: "))
-    
+    limit_output = int(input("\nMasukkan berapa banyak output yang anda inginkan untuk ditampilkan: "))
+
     password_generator = PatternBasedPasswordGenerator(length, pattern)
     passwords1 = password_generator.pattern_generate(limit_output)
     print("\nGenerated passwords with pattern:", passwords1)
-    
+
     passwords2 = password_generator.generate(limit_output)
     print("\nGenerated passwords without pattern:", passwords2)
+
+    new_passwords = []
+    for password in passwords2:
+        new_passwords.append(find_regex(password))
+    print("\nGenerated passwords without pattern and regex modification:", new_passwords)
+
 
 length = int(input("Masukkan panjang password yang diinginkan: "))
 generate_password(length)
